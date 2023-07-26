471decb9f66fca7d85e979adf6497dd60b8c2fc3
ironyCats <ironycats@gmail.com>
2023-07-26 14:37:44 +0900

    Add CarInfoActivity

---
+++ app/src/main/java/com/example/mobility/CarInfoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mobility
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CarInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_car_info)
+    }
+}
