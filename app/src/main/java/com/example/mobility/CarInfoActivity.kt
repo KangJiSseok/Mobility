4cd2c73c099de35a18e8ce0db7febf296b2e5908
ironyCats <ironycats@gmail.com>
2023-07-26 15:15:39 +0900

    RepairInfoActivity 추가

--- app/src/main/java/com/example/mobility/CarInfoActivity.kt
+++ app/src/main/java/com/example/mobility/CarInfoActivity.kt
@@ -2,10 +2,21 @@ package com.example.mobility
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mobility.databinding.ActivityCarInfoBinding
 
 class CarInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_car_info)
+        val binding = ActivityCarInfoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 차종, 연식, 주행 거리를 입력받음
+        binding.submit.setOnClickListener {
+            val car = binding.car.text.toString() // 차종
+            val year = binding.year.text.toString() // 연식
+            val odo = binding.odo.text.toString() // 주행거리
+
+            // 차량 정보 등록
+        }
     }
 }
