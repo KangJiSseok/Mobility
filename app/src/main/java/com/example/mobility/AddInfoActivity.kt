f239a065692db049ed511ed2d18c15682c42b6df
ironyCats <ironycats@gmail.com>
2023-07-26 15:43:41 +0900

    update

--- app/src/main/java/com/example/mobility/AddInfoActivity.kt
+++ app/src/main/java/com/example/mobility/AddInfoActivity.kt
@@ -1,11 +1,21 @@
 package com.example.mobility
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
+import android.app.Dialog
 import android.os.Bundle
+import android.widget.DatePicker
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mobility.databinding.ActivityAddInfoBinding
+
 
 class AddInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_info)
+        val binding = ActivityAddInfoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
+
 }
