21aadb0674fb3313407ed906d512d8fd9b6ae95f
ironyCats <ironycats@gmail.com>
2023-07-27 13:17:49 +0900

    RepairInfoActivity에 정보 전달

--- app/src/main/java/com/example/mobility/AddInfoActivity.kt
+++ app/src/main/java/com/example/mobility/AddInfoActivity.kt
@@ -36,5 +36,46 @@ class AddInfoActivity : AppCompatActivity() {
 
         // 값 전달 테스트
         Toast.makeText(this, "차종: ${car}, 연식: ${year}, 주행거리: ${odo}", Toast.LENGTH_SHORT).show()
+
+        // 등록하기 버튼 클릭 시 RepairInfoActivity 실행 및 정보 전달
+        binding.submit.setOnClickListener {
+            val intent = Intent(this, RepairInfoActivity::class.java)
+
+            intent.putExtra("car", car)
+            intent.putExtra("year", year)
+            intent.putExtra("odo", odo)
+
+            intent.putExtra("eng-odo", binding.engOdo.text.toString())
+            intent.putExtra("ac-odo", binding.acOdo.text.toString())
+            intent.putExtra("tire-odo", binding.tireOdo.text.toString())
+
+            val engDate = "${binding.engDate.year}-${monthClear(binding.engDate.month)}" +
+                    "-${dayClear(binding.engDate.dayOfMonth)}"
+            val acDate = "${binding.acDate.year}-${monthClear(binding.acDate.month)}" +
+                    "-${dayClear(binding.acDate.dayOfMonth)}"
+            val tireDate = "${binding.tireDate.year}-${monthClear(binding.tireDate.month)}" +
+                    "-${dayClear(binding.tireDate.dayOfMonth)}"
+
+            intent.putExtra("eng-date", engDate)
+            intent.putExtra("ac-date", acDate)
+            intent.putExtra("tire-date", tireDate)
+
+            startActivity(intent)
+        }
+    }
+
+    private fun monthClear(month: Int): String {
+        val month: Int = month + 1
+        if (month < 10) {
+            return "0${month}"
+        }
+        return month.toString()
+    }
+
+    private fun dayClear(day: Int): String {
+        if (day < 10) {
+            return "0${day}"
+        }
+        return day.toString()
     }
 }
