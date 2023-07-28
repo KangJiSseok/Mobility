55582442d54a1915133e0dc0e755754c35563c6a
ironyCats <ironycats@gmail.com>
2023-07-28 11:24:48 +0900

    에러 및 예외 확인

--- app/src/main/java/com/example/mobility/AddInfoActivity.kt
+++ app/src/main/java/com/example/mobility/AddInfoActivity.kt
@@ -12,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.mobility.databinding.ActivityAddInfoBinding
 import org.w3c.dom.Text
 import java.text.SimpleDateFormat
+import java.time.LocalDate
 import java.util.Calendar
 import java.util.Date
 
@@ -46,14 +47,39 @@ class AddInfoActivity : AppCompatActivity() {
             val acOdo = binding.acOdo.text.toString()
             val tireOdo = binding.tireOdo.text.toString()
 
-            // 입력 유효성 검사
-//            if (engOdo.isNullOrEmpty() || acOdo.isNullOrEmpty() || tireOdo.isNullOrEmpty()) {
-//                var loginfailalert = AlertDialog.Builder(this)
-//                loginfailalert.setMessage("모든 입력란을 채워 주세요.")
-//                loginfailalert.setPositiveButton("확인", null)
-//                loginfailalert.show()
-//                return@setOnClickListener
-//            }
+            val engDate = "${binding.engDate.year}-${monthClear(binding.engDate.month)}" +
+                    "-${dayClear(binding.engDate.dayOfMonth)}"
+            val acDate = "${binding.acDate.year}-${monthClear(binding.acDate.month)}" +
+                    "-${dayClear(binding.acDate.dayOfMonth)}"
+            val tireDate = "${binding.tireDate.year}-${monthClear(binding.tireDate.month)}" +
+                    "-${dayClear(binding.tireDate.dayOfMonth)}"
+
+            // 모든 주행 거리 입력란을 제대로 입력했는지 확인
+            if (engOdo.isNullOrEmpty() || acOdo.isNullOrEmpty() || tireOdo.isNullOrEmpty()) {
+                var loginfailalert = AlertDialog.Builder(this)
+                loginfailalert.setMessage("모든 입력란을 채워 주세요.")
+                loginfailalert.setPositiveButton("확인", null)
+                loginfailalert.show()
+                return@setOnClickListener
+            }
+
+            // 부품 교체의 주행 거리가 현재 주행 거리보다 많은 경우 에러 리턴
+            if (engOdo > odo || acOdo > odo || tireOdo > odo) {
+                var loginfailalert = AlertDialog.Builder(this)
+                loginfailalert.setMessage("부품 교체 당시의 주행 거리를 입력해 주세요.")
+                loginfailalert.setPositiveButton("확인", null)
+                loginfailalert.show()
+                return@setOnClickListener
+            }
+
+            // 부품 교체 시기가 미래인 경우 에러 리턴
+            if (isFutureDate(engDate) || isFutureDate(acDate) || isFutureDate(tireDate)) {
+                var loginfailalert = AlertDialog.Builder(this)
+                loginfailalert.setMessage("부품 교체 당시의 날짜를 입력해 주세요.")
+                loginfailalert.setPositiveButton("확인", null)
+                loginfailalert.show()
+                return@setOnClickListener
+            }
 
             val intent = Intent(this, RepairInfoActivity::class.java)
 
@@ -65,12 +91,7 @@ class AddInfoActivity : AppCompatActivity() {
             intent.putExtra("ac-odo", acOdo)
             intent.putExtra("tire-odo", tireOdo)
 
-            val engDate = "${binding.engDate.year}-${monthClear(binding.engDate.month)}" +
-                    "-${dayClear(binding.engDate.dayOfMonth)}"
-            val acDate = "${binding.acDate.year}-${monthClear(binding.acDate.month)}" +
-                    "-${dayClear(binding.acDate.dayOfMonth)}"
-            val tireDate = "${binding.tireDate.year}-${monthClear(binding.tireDate.month)}" +
-                    "-${dayClear(binding.tireDate.dayOfMonth)}"
+
 
             intent.putExtra("eng-date", engDate)
             intent.putExtra("ac-date", acDate)
@@ -93,5 +114,12 @@ class AddInfoActivity : AppCompatActivity() {
             return "0${day}"
         }
         return day.toString()
+    }
+
+    private fun isFutureDate(dateString: String): Boolean {
+        val currentDate = LocalDate.now()
+        val inputDate = LocalDate.parse(dateString)
+
+        return inputDate.isAfter(currentDate)
     }
 }
