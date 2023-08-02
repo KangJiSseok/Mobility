4c588229938800fee2bdf123e76ac0748e929b69
ironyCats <ironycats@gmail.com>
2023-08-02 16:42:34 +0900

    UpdateRepairActivity 업데이트

--- app/src/main/java/com/example/mobility/activity/UpdateRepairActivity.kt
+++ app/src/main/java/com/example/mobility/activity/UpdateRepairActivity.kt
@@ -1,8 +1,12 @@
 package com.example.mobility.activity
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
 import com.example.mobility.MyApplication
 import com.example.mobility.R
 import com.example.mobility.databinding.ActivityUpdateRepairBinding
@@ -12,10 +16,12 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
+import java.time.LocalDate
 
 class UpdateRepairActivity : AppCompatActivity() {
     lateinit var binding: ActivityUpdateRepairBinding
     var data = ItemData()
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityUpdateRepairBinding.inflate(layoutInflater)
@@ -30,6 +36,13 @@ class UpdateRepairActivity : AppCompatActivity() {
 
         binding.infoTitle.text = "${getParticle(part.toString(), "을", "를")}\n언제 교체하셨나요?"
 
+        var code = ""
+        when (part) {
+            "엔진오일" -> code = "engine"
+            "에어컨 필터" -> code = "ac"
+            "타이어" -> code = "tire"
+        }
+
         CoroutineScope(Dispatchers.Main).launch {
             val documents = withContext(Dispatchers.IO) {
                 MyApplication.db.collection(MyApplication.auth.currentUser!!.uid).get().await()
@@ -42,14 +55,64 @@ class UpdateRepairActivity : AppCompatActivity() {
                 }
             }
 
-            var code = ""
-            when (part) {
-                "엔진오일" -> code = "engine"
-                "에어컨 필터" -> code = "ac"
-                "타이어" -> code = "tire"
-            }
             binding.odo.text = "이전 주행 거리: ${data.RepairInfo["${code}Odo"]} km\n이전 교체 일자: ${data.RepairInfo["${code}Date"]}"
         }
+
+        binding.submit.setOnClickListener {
+            val odo = binding.newOdo.text.toString().trim()
+            val date = "${binding.newDate.year}-${monthClear(binding.newDate.month)}" +
+                    "-${dayClear(binding.newDate.dayOfMonth)}"
+
+            if (odo.isEmpty()) {
+                var loginfailalert = AlertDialog.Builder(this)
+                loginfailalert.setMessage("모든 입력란을 채워 주세요.")
+                loginfailalert.setPositiveButton("확인", null)
+                loginfailalert.show()
+                return@setOnClickListener
+            }
+
+            // 부품 교체 시기가 미래인 경우 에러 리턴
+            if (isFutureDate(date)) {
+                var loginfailalert = AlertDialog.Builder(this)
+                loginfailalert.setMessage("부품 교체 당시의 날짜를 입력해 주세요.")
+                loginfailalert.setPositiveButton("확인", null)
+                loginfailalert.show()
+                return@setOnClickListener
+            }
+
+            data.RepairInfo["${code}Odo"] = odo
+            data.RepairInfo["${code}Date"] = date
+            MyApplication.db.collection(MyApplication.auth.currentUser!!.uid).document("RepairInfo").update(data.RepairInfo as Map<String, Any>)
+                .addOnCompleteListener{
+                    if (it.isSuccessful){
+                        Toast.makeText(applicationContext, "성공", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            finish()
+        }
+    }
+
+    private fun monthClear(month: Int): String {
+        val month: Int = month + 1
+        if (month < 10) {
+            return "0${month}"
+        }
+        return month.toString()
+    }
+
+    private fun dayClear(day: Int): String {
+        if (day < 10) {
+            return "0${day}"
+        }
+        return day.toString()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun isFutureDate(dateString: String): Boolean {
+        val currentDate = LocalDate.now()
+        val inputDate = LocalDate.parse(dateString)
+
+        return inputDate.isAfter(currentDate)
     }
 
     // 한글 받침에 따른 조사 처리
