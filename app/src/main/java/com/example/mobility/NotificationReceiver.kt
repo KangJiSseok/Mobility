1a328303e6cb3a57680230b020bf636fa7f5f6e4
hhyukjin <80118226+hhyukjin@users.noreply.github.com>
2023-08-06 00:30:49 +0900

    지정시간 알림 기능 추가
    주행거리 업데이트 코드 정리 및 지정한 시간에 알림오는 기능 추가

---
+++ app/src/main/java/com/example/mobility/NotificationReceiver.kt
@@ -0,0 +1,70 @@
+package com.example.mobility
+
+import android.R
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.example.mobility.activity.MainActivity
+
+
+class NotificationReceiver : BroadcastReceiver() {
+
+    var manager: NotificationManager? = null
+    var builder: NotificationCompat.Builder? = null
+
+    //오레오 이상은 반드시 채널을 설정해줘야 Notification이 작동함
+    private val CHANNEL_ID = "ch1"
+    private val CHANNEL_NAME = "Channel1"
+
+    override fun onReceive(context: Context, intent: Intent) {
+
+        Log.e("kkk","ok")
+        manager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        builder = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            manager!!.createNotificationChannel(
+                NotificationChannel(
+                    CHANNEL_ID,
+                    CHANNEL_NAME,
+                    NotificationManager.IMPORTANCE_DEFAULT
+                )
+            )
+            NotificationCompat.Builder(context, CHANNEL_ID)
+        } else {
+            NotificationCompat.Builder(context)
+        }
+
+        val intent = Intent(context, MainActivity::class.java)
+
+
+        val pendingIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            PendingIntent.getActivity( context, 101, intent, PendingIntent.FLAG_MUTABLE)
+        } else {
+            PendingIntent.getActivity( context, 101, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        }
+
+        //알림창 제목
+        builder!!.setContentTitle("test") //회의명노출
+
+        //알림창 아이콘
+        builder!!.setSmallIcon(R.drawable.alert_dark_frame)
+
+        //알림창 터치시 자동 삭제
+        builder!!.setAutoCancel(true)
+
+        builder!!.setContentIntent(pendingIntent)
+
+        //푸시알림 빌드
+        val notification: Notification = builder!!.build()
+
+        //NotificationManager를 이용하여 푸시 알림 보내기
+        manager!!.notify(1, notification)
+    }
+}
--- app/src/main/java/com/example/mobility/activity/UpdateOdoActivity.kt
+++ app/src/main/java/com/example/mobility/activity/UpdateOdoActivity.kt
@@ -1,5 +1,9 @@
 package com.example.mobility.activity
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,6 +12,7 @@ import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import com.example.mobility.MyApplication
+import com.example.mobility.NotificationReceiver
 import com.example.mobility.R
 import com.example.mobility.databinding.ActivityUpdateOdoBinding
 import com.example.mobility.model.ItemData
@@ -21,15 +26,19 @@ import java.time.LocalDate
 import java.util.Calendar
 
 class UpdateOdoActivity : AppCompatActivity() {
-    var data = ItemData()
-    lateinit var binding: ActivityUpdateOdoBinding
+    private lateinit var alarmManager:AlarmManager
+
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityUpdateOdoBinding.inflate(layoutInflater)
+        val binding = ActivityUpdateOdoBinding.inflate(layoutInflater)
         setContentView(binding.root)
         title = "주행 거리 업데이트"
         supportActionBar?.setDisplayHomeAsUpEnabled(true);
+
+        alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+
+        var data = ItemData()
 
         CoroutineScope(Dispatchers.Main).launch {
             val documents = withContext(Dispatchers.IO) {
@@ -67,8 +76,8 @@ class UpdateOdoActivity : AppCompatActivity() {
                 return@setOnClickListener
             }
 
-            val Today: LocalDate = LocalDate.now()
-            data.CarInfo["lastDate"] = Today.toString()
+            val today: LocalDate = LocalDate.now()
+            data.CarInfo["lastDate"] = today.toString()
             data.CarInfo["odo"] = binding.odo.text.toString()
             MyApplication.db.collection(MyApplication.auth.currentUser!!.uid).document("CarInfo").update(data.CarInfo as Map<String, Any>)
                 .addOnCompleteListener{
@@ -76,6 +85,9 @@ class UpdateOdoActivity : AppCompatActivity() {
                         Toast.makeText(applicationContext, "주행 거리가 업데이트 되었습니다.", Toast.LENGTH_SHORT).show()
                     }
                 }
+            CoroutineScope(Dispatchers.Default).launch {
+                setNotification(today.toString())
+            }
             finish()
         }
     }
@@ -93,28 +105,28 @@ class UpdateOdoActivity : AppCompatActivity() {
         return diffDate.toInt()
     }
 
-    override fun onResume() {
-        super.onResume()
-        CoroutineScope(Dispatchers.Main).launch {
-            val documents = withContext(Dispatchers.IO) {
-                MyApplication.db.collection(MyApplication.auth.currentUser!!.uid).get().await()
-            }
-            for (document in documents) {
-                when (document.id) {
-                    "CarInfo" -> data.CarInfo = document.data as HashMap<String, String>
-                    "RepairInfo" -> data.RepairInfo = document.data as HashMap<String, String>
-                    "Profile" -> data.Profile = document.data as HashMap<String, String>
-                }
-            }
-
-            binding.previousOdo.text = "이전 주행 거리: ${data.CarInfo["odo"]} km"
-        }
-    }
-
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             android.R.id.home -> finish()
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun setNotification(date: String){
+        val intent = Intent(this, NotificationReceiver::class.java)
+
+        val pendingIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            PendingIntent.getBroadcast(this,111,intent,PendingIntent.FLAG_MUTABLE )
+        } else {
+            PendingIntent.getBroadcast(this,111,intent,PendingIntent.FLAG_UPDATE_CURRENT )
+        }
+
+        val calender = Calendar.getInstance()
+        val alarmDate = "$date 15:28:00"
+        val sf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+        calender.time = sf.parse(alarmDate)
+//        calender.add(Calendar.DATE, 1)
+
+        alarmManager.set(AlarmManager.RTC, calender.timeInMillis, pendingIntent);
     }
 }
