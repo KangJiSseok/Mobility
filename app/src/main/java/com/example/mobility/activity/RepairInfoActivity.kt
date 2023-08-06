b3671c36c968d2fe2a1da11fb32968e4e81fb3fd
강지석 <rkdwltjr5876@naver.com>
2023-08-06 22:16:10 +0900

    google place api 추가

--- app/src/main/java/com/example/mobility/activity/RepairInfoActivity.kt
+++ app/src/main/java/com/example/mobility/activity/RepairInfoActivity.kt
@@ -33,6 +33,12 @@ class RepairInfoActivity : AppCompatActivity() {
 
         title = "남은 주행 거리와 기간"
 
+        //구글맵으로 이동 추가
+        binding.btnGoogleMap.setOnClickListener {
+            val intent = Intent(this, GoogleMapActivity::class.java)
+            startActivity(intent)
+        }
+
         binding.carInfoLayout.setOnClickListener {
             val intent = Intent(this, CarInfoActivity::class.java)
             startActivity(intent)
